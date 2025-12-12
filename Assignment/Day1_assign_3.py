1a6e63b089ba924a33635ce87f97c7bc5f3084af
AdityaPatil81 <23uet081@dkte.ac.in>
2025-12-12 12:46:49 +0530

    Day1 assignments

---
+++ Assignment/Day1_assign_3.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+
+print("\nbefore Swapping-\n")
+
+print("a : ",num1)
+print("b : ",num2)
+
+print("\nAfter Swapping-\n")
+
+temp = num1
+num1 = num2 
+num2 = temp
+
+print("a : ",num1)
+print("b : ",num2)
---
+++ Assignment/Day1_assign_4.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+
+add = int(num1 + num2)
+sub = int(num1 - num2)
+div = float(num1 / num2)
+mul = int(num1 * num2)
+
+print("Addition : ",add)
+print("subtraction : ",sub)
+print("Division : ",div)
+print("Multiplication : ",mul)
